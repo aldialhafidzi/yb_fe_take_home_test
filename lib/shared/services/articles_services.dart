a4660e277364f698610380b93028f2e38ce5a179
Aldi Alhafidzi <aldi.alhafidzi@gmail.com>
2026-01-18 16:36:37 +0700

    update env news

--- lib/shared/services/articles_services.dart
+++ lib/shared/services/articles_services.dart
@@ -4,12 +4,10 @@ import 'package:http/http.dart' as http;
 import '../models/article.dart';
 
 class ArticlesServices {
-  static const String apiKey = 'ecab2a5d54bed16048ed4f5b8ca57506';
-  // static const String apiKey = '3c47279cd5c0b54f35a53bc5ae0cbe69';
-  static const String baseUrl = 'https://gnews.io/api/v4';
+  static const String baseUrl = 'https://gnews-proxy-test.vercel.app/api';
 
   Future<List<Article>> fetchArticles(String query, String max) async {
-    final url = Uri.parse('$baseUrl/search?max=$max&q=$query&lang=en&apikey=$apiKey');
+    final url = Uri.parse('$baseUrl/search?max=$max&q=$query');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
@@ -23,7 +21,7 @@ class ArticlesServices {
 
   Future<List<Article>> fetchTopArticles(String category) async {
     final url = Uri.parse(
-      '$baseUrl/top-headlines?max=5&category=$category&lang=en&apikey=$apiKey',
+      '$baseUrl/top-headlines?max=5&category=$category',
     );
     final response = await http.get(url);
 
